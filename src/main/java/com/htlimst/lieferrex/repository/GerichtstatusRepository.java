c002fc76d62945cf0d5e85ae098774f4ee9cd0fc
Michi <bogensbergermichael@gmail.com>
2022-02-21 15:34:09 +0100

    changed gericht status

--- src/main/java/com/htlimst/lieferrex/repository/GerichtstatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.htlimst.lieferrex.repository;
-
-import com.htlimst.lieferrex.model.Gerichtstatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GerichtstatusRepository extends JpaRepository<Gerichtstatus,Long> {
-}
