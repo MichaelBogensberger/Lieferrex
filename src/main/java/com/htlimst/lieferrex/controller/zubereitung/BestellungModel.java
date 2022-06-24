137f50a2995b43fc1454c37818fd093451248d76
Ming <ming.ming@live.de>
2022-06-24 17:03:27 +0200

    SUI changes

--- src/main/java/com/htlimst/lieferrex/controller/zubereitung/BestellungModel.java
+++ src/main/java/com/htlimst/lieferrex/controller/zubereitung/BestellungModel.java
@@ -17,11 +17,13 @@ public class BestellungModel {
     HashMap<String, Integer> gerichtBestellungModelList;
     String timestamp;
     String zusatzinfos;
+    int counter;
 
-    public BestellungModel(HashMap<String, Integer> gerichtBestellungModelList, String time, String zusatz) {
+    public BestellungModel(HashMap<String, Integer> gerichtBestellungModelList, String time, String zusatz, int counter) {
 
         this.gerichtBestellungModelList = gerichtBestellungModelList;
         this.timestamp = time;
         this.zusatzinfos = zusatz;
+        this.counter = counter;
     }
 }
