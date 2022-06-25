afc90d3aeaf03e919089f1dc92ba4bb3ded8d7c7
Ming <ming.ming@live.de>
2022-06-25 18:04:14 +0200

    Zubereitungs page ready

--- src/main/java/com/htlimst/lieferrex/repository/BestellungRepository.java
+++ src/main/java/com/htlimst/lieferrex/repository/BestellungRepository.java
@@ -1,5 +1,6 @@
 package com.htlimst.lieferrex.repository;
 
+import com.htlimst.lieferrex.model.Bestellstatus;
 import com.htlimst.lieferrex.model.Bestellung;
 import com.htlimst.lieferrex.model.Mandant;
 import com.htlimst.lieferrex.model.Kunde;
@@ -15,7 +16,7 @@ public interface BestellungRepository extends JpaRepository<Bestellung,Long> {
     @Query(value = "SELECT * FROM bestellung as b INNER JOIN kunde as k ON b.kunde_id = k.kunde_id INNER JOIN bestellart as ba ON b.bestellart_id = ba.bestellart_id WHERE b.mandant_id = ?1 ORDER BY bestelldatum DESC LIMIT 3  ", nativeQuery = true)
     List<Bestellung> getLatestThreeEntries(long mandantId);
 
-    List<Bestellung> getBestellungByMandant_Id(long mandantId);
+    List<Bestellung> getBestellungByMandant_IdAndBestellstatus(long mandantId, Bestellstatus bestellstatus);
 
 
     Bestellung getBestellungById(long bestellId);
